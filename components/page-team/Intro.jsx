7ea161d3ce05b90cc5138c9bc85d9465e39a896a
leisha1703 <leisha1703@gmail.com>
2025-05-27 12:23:48 +0530

    Today commit

--- components/page-team/Intro.jsx
+++ components/page-team/Intro.jsx
@@ -30,7 +30,7 @@ function Intro() {
               <div className="skills-box row mt-80">
                 <div className="col-md-6">
                   <div className="skill-item sm-mb30">
-                    <h5 className="sub-title mb-15">UI / UX Design 90%</h5>
+                    <h5 className="sub-title mb-15">UI / UX Design 95%</h5>
                     <div className="skill-progress">
                       <div className="progres" data-value="90%"></div>
                     </div>
@@ -38,7 +38,7 @@ function Intro() {
                 </div>
                 <div className="col-md-6">
                   <div className="skill-item">
-                    <h5 className="sub-title mb-15">Development 80%</h5>
+                    <h5 className="sub-title mb-15">Development 90%</h5>
                     <div className="skill-progress">
                       <div className="progres" data-value="80%"></div>
                     </div>
@@ -50,13 +50,13 @@ function Intro() {
           <div className="col-lg-6">
             <div className="imgs">
               <div className="img1">
-                <img src="/assets/imgs/intro/2.jpg" alt="" />
+                <img src="/assets/imgs/intro/intro1.jpg" alt="" />
               </div>
               <div className="img2">
-                <img src="/assets/imgs/intro/03.jpg" alt="" />
+                <img src="/assets/imgs/intro/intro2.jpg" alt="" />
               </div>
               <div className="img3">
-                <img src="/assets/imgs/intro/04.jpg" alt="" />
+                <img src="/assets/imgs/intro/intro3.jpg" alt="" />
               </div>
             </div>
           </div>
