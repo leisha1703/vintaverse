6c6c994e37cfb80ae873a7e083901816be8b5808
leisha1703 <leisha1703@gmail.com>
2025-05-30 14:26:55 +0530

    Portfolio section updated, responsive check,services card layout of border changed with rotating animation, undo to as it was before

--- components/RotatingCard.jsx
+++ components/RotatingCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './RotatingCard.css';
 const RotatingCard = () => {
-  return (
-    <div className="card">
-      <span className="card-border" />
-      <span className="card-mask" />
-      <div className="content">Rotating Border</div>
-    </div>
-  );
+    return (
+        <>
+
+
+
+            <div className="snake-border">
+                Your Content Here
+            </div>
+
+        </>
+
+    );
 };
 export default RotatingCard;
